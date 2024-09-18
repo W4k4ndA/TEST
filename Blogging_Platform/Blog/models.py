c0deb56caf514c3e94f6bfcdfa6c7f2cfb58c32c
W4k4ndA <jose.manuel.guerra.mayora@gmail.com>
2024-09-18 12:58:40 -0400

    create the blog model for the app

--- Blogging_Platform/Blog/models.py
+++ Blogging_Platform/Blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.CharField(max_length=255)
+    tags = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'

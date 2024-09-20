8d698bd0573f6d50c8b666856b0b72ca7e869c46
W4k4ndA <jose.manuel.guerra.mayora@gmail.com>
2024-09-20 10:34:23 -0400

    update models.py and db

--- Blogging_Platform/Blog/models.py
+++ Blogging_Platform/Blog/models.py
@@ -7,7 +7,8 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     category = models.CharField(max_length=255)
-    tags = models.CharField(max_length=255)
+    # tags = models.CharField(max_length=255)
+    tags = models.JSONField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
